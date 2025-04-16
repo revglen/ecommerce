43694ab7238bc61396c2214d5da21d83b26d3d66
vboxuser <vboxuser@ubuntu1.myguest.virtualbox.org>
2025-04-16 20:04:50 +0100

    updated files

--- gateway-service/outputs.tf
+++ gateway-service/outputs.tf
@@ -6,6 +6,3 @@ output "instance_name" {
 #   value = google_compute_instance.docker_vm.network_interface[0].access_config[0].nat_ip
 # }
 
-output "firewall_status" {
-  value = length(data.google_compute_firewall.existing.*.name) == 0 ? "Created new rule" : "Rule already existed (no changes)"
-}
