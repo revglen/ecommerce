de624d7b03c7109cc4e47ca29821a8c01fe5f297
vboxuser <vboxuser@ubuntu1.myguest.virtualbox.org>
2025-04-16 12:31:08 +0100

    updated files

--- gateway-service/gcp_firewall.tf
+++ gateway-service/gcp_firewall.tf
@@ -14,3 +14,8 @@ resource "google_compute_firewall" "allow_web_traffic" {
   source_ranges = ["0.0.0.0/0"]
   target_tags   = ["http-server", "https-server"]
 }
+
+import {
+  to = google_compute_firewall.allow_web_traffic
+  id = "projects/testo-455513/global/firewalls/allow-web-traffic"
+}
