7e4f706c695c0f4e7de9d81034aaffbbf80dc60a
vboxuser <vboxuser@ubuntu1.myguest.virtualbox.org>
2025-04-17 21:44:32 +0100

    files updated

--- order-service/init-db.sh
+++ order-service/init-db.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+psql -v ON_ERROR_STOP=1 --U postgres -d postgres <<-EOSQL
     CREATE USER order_user WITH PASSWORD 'order_password';
     CREATE DATABASE "order_db" WITH OWNER order_user;
     GRANT ALL PRIVILEGES ON DATABASE "order_db" TO order_user;
