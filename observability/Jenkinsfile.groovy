66dea1624513f8ca1baefb45f1ac3d246a17252e
vboxuser <vboxuser@ubuntu1.myguest.virtualbox.org>
2025-04-18 15:06:22 +0100

    Database debugging

--- observability/Jenkinsfile.groovy
+++ observability/Jenkinsfile.groovy
@@ -4,20 +4,24 @@ def call(Map params) {
     env.GCP_PROJECT = params.gcpProject
     env.GITHUB_REPO = params.githubRepo
     env.ZONE = params.zone
-    env.REGISTRY = params.registry
-    env.CONSUL_IP = params.consulIP
     env.WORKSPACE = params.workspace
+    env.GOOGLE_CREDENTIALS = params.google_credentials
+    env.TF_VAR_project_id = params.gcpProject
+    env.SSH_KEY=params.ssh_key
+    env.SSH_PUB_KEY=params.ssh_pub_key
+    env.SECRET_KEY=params.secret_key
+    env.API_KEY=params.api_key
+    env.ALGORITHM=params.algorithm
+    env.ACCESS_TOKEN_EXPIRE_MINUTES=params.token_expiry
+    env.CONSUL_ADDRESS = params.result 
 
     def COMPOSE_FILE = 'docker-compose.yml'
-    def CONSUL_IP = sh(
-            script: "hostname -I | awk '{print \$1}'",
-            returnStdout: true
-        ).trim()
+    def CONSUL_IP = params.result 
     
     stage('Build observability Service') {        
 
         echo "${env.WORKSPACE}/observability//${COMPOSE_FILE}"
-        sh "echo \"CONSUL_HOST=${env.CONSUL_IP}\" > ${env.WORKSPACE}/observability/.env"
+        sh "echo \"CONSUL_HOST=${CONSUL_IP}\" > ${env.WORKSPACE}/observability/.env"
         sh "echo \"CONSUL_PORT=8500\" >> ${env.WORKSPACE}/observability/.env"
         sh "echo \"PROMETHEUS_ENABLED=true\" >> ${env.WORKSPACE}/observability/.env"
         sh "echo \"LOG_LEVEL=INFO\" >> ${env.WORKSPACE}/observability/.env"
@@ -25,13 +29,13 @@ def call(Map params) {
         sh "docker compose -f ${env.WORKSPACE}/observability/${COMPOSE_FILE} up -d --build"
     }
     
-    stage('Tag and Push Gateway Images') {
+    stage('Tag and Push auth Images') {
        
         def DOCKER_CONFIG = "${env.DOCKER_HOME}/.docker"
         def HOME = "${env.DOCKER_HOME}"    
         
         def services = sh(
-            script: "docker compose -f ${env.WORKSPACE}/observability//${COMPOSE_FILE} config --services",
+            script: "docker compose -f ${env.WORKSPACE}/observability/${COMPOSE_FILE} config --services",
             returnStdout: true
         ).trim().split('\n')
                 
@@ -39,37 +43,76 @@ def call(Map params) {
             echo "--- Processing service: ${service} ---"
         
             def sourceImage = sh(
-                script: "docker compose -f ${env.WORKSPACE}/observability/${COMPOSE_FILE} config | grep -A5 '${service}' | grep 'image:' | awk '{print \$2}'",
+                script: "docker compose -f ${env.WORKSPACE}/observability/${COMPOSE_FILE} config | grep -A15 '${service}:' | grep 'image:' | awk '{print \$2}'",
                 returnStdout: true
             ).trim()
             
             echo "Source image from compose: '${sourceImage}'"
-            
-            def imageNameOnly = sourceImage.split('/').last()
-            def targetImage = "${REGISTRY}/${imageNameOnly}-${env.BUILD_NUMBER}"
-            
-            echo "Calculated target image: '${targetImage}'"
-            
-            def imageId = sh(
-                script: "docker images -q ${sourceImage}",
-                returnStdout: true
-            ).trim()
-            
-            if (!imageId) {
-                echo "WARNING: Source image not found! Listing available images:"
-                sh 'docker images'
-                error "Source image ${sourceImage} not found in local registry!"
-            }
-            
+                                          
+            // Save the Docker image as a tar file
             sh """
-            docker tag ${sourceImage} ${targetImage}
+                docker save ${sourceImage} -o ${service}.tar
+                echo "The docker saved to ${service}.tar"
+            """
+
+            def IP = sh(script: 'terraform output -raw instance_ip', returnStdout: true).trim()
+
+            // Wait for port 22 to be open
+            sh """
+                for i in \$(seq 1 10); do
+                    if nc -z -w 5 ${IP} 22; then
+                        echo "Port 22 is open, attempting SCP..."
+                        if scp -o StrictHostKeyChecking=no \
+                            -o ConnectTimeout=10 \
+                            -i ${env.SSH_KEY} \
+                            ${service}.tar .env ubuntu@${IP}:/home/ubuntu/; then
+                            echo "Successfully copied to GCP VM"
+                            break
+                        else
+                            echo "SCP attempt \$i failed"
+                        fi
+                    else
+                        echo "SSH not available yet (attempt \$i/10)"
+                    fi
+                    sleep 5
+                done
+            """
+
+            def parameters = ""
+
+            if (service != "postgres") {
+                parameters = "--name observability-service --network observability-net -p 8001:8001"
+            } else {
+                parameters = "--name observability-postgres --network observability-net -e POSTGRES_USER=observability_user -e POSTGRES_PASSWORD=observability_password -e POSTGRES_DB=observability_db -e POSTGRES_HOST_AUTH_METHOD=trust -p 5432:5432"
+            }
+
+            // Wait for port 22 to be open
+            sh """
+                for i in \$(seq 1 10); do
+                    if nc -z "$IP" 22; then
+                        echo "Port 22 is open for Docker command execution."
+                        
+                        if ssh -o StrictHostKeyChecking=no -i "$SSH_KEY" "ubuntu@$IP" \
+                        "docker load -i /home/ubuntu/${service}.tar && pwd && docker run --env-file ./.env -d -p 8002:8002 $sourceImage"; then
+                            echo "Docker commands executed successfully."
+                            break
+                        else
+                            echo "Failed to execute Docker commands. Retrying..."
+                            sleep 5
+                        fi
+                    else
+                        echo "Waiting for SSH to be available..."
+                        sleep 5
+                    fi
+                done
             """
             
-            //sh """
-            //docker push ${targetImage}
-            //"""
+            sh """            
+                rm -rf ${service}.tar
+                echo "Deleted the tar file ${service}.tar"
+            """                                
         }
-    }
+    }   
     
     stage('Cleanup observability/ Containers') {
         echo "Stopping observability/ containers"
