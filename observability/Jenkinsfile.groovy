8885feac311c75a11e77b00887271399a107ad1e
vboxuser <vboxuser@ubuntu1.myguest.virtualbox.org>
2025-04-18 20:11:26 +0100

    Database debugging

--- observability/Jenkinsfile.groovy
+++ observability/Jenkinsfile.groovy
@@ -119,11 +119,11 @@ def call(Map params) {
                         echo "Port 22 is open for Docker command execution."
                         
                         if ssh -o StrictHostKeyChecking=no -i "$SSH_KEY" "ubuntu@$IP" \
-                        "docker load -i /home/ubuntu/${service}.tar && pwd && docker run --env-file ./.env -d -p 8002:8002 $sourceImage"; then
+                        "docker load -i /home/ubuntu/${service}.tar && docker run --env-file ./.env -d $parameters $sourceImage"; then
                             echo "Docker commands executed successfully."
                             break
                         else
-                            echo "Failed to execute Docker commands. Retrying..."
+                            echo "Failed to execute Docker commands. Retrying attempt \$i/10"
                             sleep 5
                         fi
                     else
