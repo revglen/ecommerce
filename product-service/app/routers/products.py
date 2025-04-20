b6cc71b18d1b376050462ca7591a7762f6029651
vboxuser <vboxuser@ubuntu1.myguest.virtualbox.org>
2025-04-20 12:16:27 +0100

    files updated

--- product-service/app/routers/products.py
+++ product-service/app/routers/products.py
@@ -35,7 +35,8 @@ async def create_product(
 
 @router.get(
     "/",
-    response_model=schemas.ProductListResponse
+    response_model=schemas.ProductListResponse,
+    status_code=status.HTTP_200_OK
 )
 async def read_products(
     skip:int =0,
@@ -66,7 +67,8 @@ async def read_products(
       
 @router.get(
     "/{product_id}", 
-    response_model=schemas.ProductResponse
+    response_model=schemas.ProductResponse,
+    status_code=status.HTTP_200_OK
 )
 async def read_product(product_id: int, db: Session = Depends(get_db)):
     try:
@@ -84,3 +86,43 @@ async def read_product(product_id: int, db: Session = Depends(get_db)):
     except Exception as e:
         logger.error(f"Get product error: {str(e)}")
         raise
+
+
+@router.put("/{product_id}", 
+            response_model=schemas.ProductResponse,
+            status_code=status.HTTP_200_OK)
+def update_product(
+    product_id: int, 
+    product: schemas.ProductUpdate, 
+    db: Session = Depends(get_db)
+):    
+    try:
+        db_product = crud.get_product(db, product_id=product_id)
+        logger.info("Product updated successfully")
+        
+        if db_product is None:
+            raise HTTPException(status_code=404, detail="Product not found")
+    
+        return crud.update_product(db=db, product_id=product_id, product=product)
+    except Exception as e:
+        logger.error(f"Uodate product error: {str(e)}")
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Could not update product"
+        )
+
+@router.delete("/{product_id}", 
+               response_model=schemas.Product,
+               status_code=status.HTTP_200_OK)
+def delete_product(product_id: int, db: Session = Depends(get_db)):
+    try:
+        db_product = crud.get_product(db, product_id=product_id)
+        if db_product is None:
+            raise HTTPException(status_code=404, detail="Product not found")
+        return crud.delete_product(db=db, product_id=product_id)
+    except Exception as e:
+        logger.error(f"Delete product error: {str(e)}")
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Could not delete product"
+        )
